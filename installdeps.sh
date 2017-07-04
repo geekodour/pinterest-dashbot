68adfa1c5cb8da9b452b9b26c866a7b4af035d25
root <root@pin.localdomain>
2017-07-04 07:12:42 +0000

    yesss

--- installdeps.sh
+++ installdeps.sh
@@ -6,6 +6,7 @@ cd /usr/bin/
 wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
 tar xvf phantomjs-2.1.1-linux-x86_64.tar.bz2
 cp phantomjs-2.1.1-linux-x86_64/bin/phantomjs .
+cd ~
 }
 getPhantom
 pip3 install virtualenv
--- src/server.py
+++ src/server.py
@@ -1,6 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
+# celery config
+app.config['CELERY_BROKER_URL'] = 'amqp://pin:pinpin@localhost/pinvh'
+app.config['CELERY_RESULT_BACKEND'] = 'amqp://pin:pinpin@localhost/pinvh'
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])  
+celery.conf.update(app.config)  
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@celery.task()
+def schedule_post(self, path):  
+    return result
