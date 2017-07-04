7af5b4898ec67e0a5f6216862cd85d52c7e7ebf8
root <root@pin.localdomain>
2017-07-04 08:11:27 +0000

    yesss

--- src/server.py
+++ src/server.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from tasks import make_celery
 app = Flask(__name__)
+
 # celery config
-app.config['CELERY_BROKER_URL'] = 'amqp://pin:pinpin@localhost/pinvh'
-app.config['CELERY_RESULT_BACKEND'] = 'amqp://pin:pinpin@localhost/pinvh'
-celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])  
-celery.conf.update(app.config)  
+app.config.update(
+    CELERY_BROKER_URL='amqp://pin:pinpin@localhost/pinvh',
+    CELERY_RESULT_BACKEND='amqp://pin:pinpin@localhost/pinvh'
+)
+celery = make_celery(app)
+#celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])  
+#celery.conf.update(app.config)  
 
 @app.route('/')
 def hello_world():
@@ -14,3 +19,6 @@ def hello_world():
 @celery.task()
 def schedule_post(self, path):  
     return result
+
+if __name__ == '__main__':  
+    app.run(port=8889, debug=True)
--- src/tasks.py
+++ src/tasks.py
@@ -6,8 +6,21 @@ $ sudo rabbitmqctl set_permissions -p pinvh pin ".*" ".*" ".*"
 """
 from celery import Celery
 
-app = Celery('tasks', broker='amqp://pin:pinpin@localhost/pinvh')
+def make_celery(app):
+    celery = Celery(app.import_name, backend=app.config['CELERY_RESULT_BACKEND'],
+                    broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
 
+# app = Celery('tasks', broker='amqp://pin:pinpin@localhost/pinvh')
+"""
 @app.task
 def add(x, y):
-    return x + y
+    return x + y"""
