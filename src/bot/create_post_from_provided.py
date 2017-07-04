4cda6b38a413d6f863027263487d73b99188c193
root <root@pin.localdomain>
2017-07-04 18:40:36 +0000

    added upload validation

--- src/bot/create_post_from_provided.py
+++ src/bot/create_post_from_provided.py
@@ -11,11 +11,16 @@ with open('done_pins.json') as json_data:
     done_pins = json.load(json_data)
 
 # generate posts to post
-available_posts = list(filter(lambda x: x['imageUrl'] in done_pins ,bot_config['posts']))
+pin_upload_count = bot_config['periods']['post_to_post_per_day']
+available_posts = list(filter(lambda x: x['imageUrl'] not in done_pins ,bot_config['posts']))
 
-for post in random(available_posts,bot_config['periods']['post_to_post_per_day']):
-    bot.createPost(post['imageUrl'],post['note'])
-    done_pins.append(post['imageUrl'])
+if len(available_posts) <= pin_upload_count:
+    pin_upload_count = len(available_posts)
 
-with open('done_pins.json', 'w') as outfile:  
-    json.dump(done_pins, outfile)
+if len(available_posts)>0:
+    for post in random.sample(available_posts,pin_upload_count):
+        bot.createPost(post['imageUrl'],post['note'])
+        done_pins.append(post['imageUrl'])
+    
+    with open('done_pins.json', 'w') as outfile:  
+        json.dump(done_pins, outfile)
